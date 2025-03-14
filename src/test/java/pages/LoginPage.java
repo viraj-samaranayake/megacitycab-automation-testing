5ce795a1735ca2fdcb12a305e3620069065d3a19
codevrj <isuruviraj25@gmail.com>
2025-03-14 23:09:29 +0530

    Login test

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,51 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+    WebDriver driver;
+
+    @FindBy(id = "email")
+    WebElement emailInput;
+
+    @FindBy(id = "password")
+    WebElement passwordInput;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement loginButton;
+
+    @FindBy(xpath = "//p[contains(@class, 'text-yellow-500')]")
+    WebElement statusMessage;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterUsername(String username) {
+        emailInput.clear();
+        emailInput.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        loginButton.click();
+    }
+
+    public String getStatusMessage() {
+        return statusMessage.getText();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,41 @@
 package tests;
 
-public class LoginTest {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUpTest() {
+        driver.get("http://localhost:5173/login");
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test(priority = 1, description = "Verify successful login with valid credentials")
+    public void testValidLogin() {
+        loginPage.login("jhn@gmail.com", "123456");
+        Assert.assertFalse(loginPage.getStatusMessage().contains("Invalid"), "Login failed with valid credentials!");
+    }
+
+    @Test(priority = 2, dataProvider = "invalidCredentials", description = "Verify unsuccessful login with invalid credentials")
+    public void testInvalidLogin(String username, String password) {
+        loginPage.login(username, password);
+        Assert.assertTrue(loginPage.getStatusMessage().contains("Invalid"), "Login succeeded with invalid credentials!");
+    }
+
+    @DataProvider(name = "invalidCredentials")
+    public Object[][] getInvalidCredentials() {
+        return new Object[][] {
+                {"invaliduser@example.com", "123456"},
+                {"jhn@gmail.com", "WrongPassword"},
+                {"", "12345"}, // Empty username
+                {"jhn@gmail.com", ""}, // Empty password
+                {"", ""} // Both empty
+        };
+    }
 }
